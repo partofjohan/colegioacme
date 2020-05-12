fc428bb960a56e3437817ed3c2d63134ef6c6fc6
Part of Johan <johanfelipe29@gmail.com>
2020-05-12 11:27:15 -0500

    Some responsive features

--- JS/index.js
+++ JS/index.js
@@ -1,5 +1,37 @@
+var element = document.getElementById("elements")
+console.log(element)
+var elementos = element.getElementsByTagName("span").length
+console.log(elementos)
+
 var flag_menu=0
 var mql = window.matchMedia('(max-width: 600px)');
+var pos=1
+
+function left(){
+  if(pos==1){
+    rote(5)
+  }else{
+    rote(pos-1)
+  }
+}
+
+function rigth(){
+  if(pos==5){
+    rote(1)
+  }else{
+    rote(pos+1)
+  }
+}
+
+function rote(index){
+  pos=index
+  console.log(index)
+  for (var i=0;i<document.getElementsByClassName("circle").length;i+=1){
+    document.getElementsByClassName("circle")[i].classList.remove("circle-fill");
+  }
+  document.getElementById("imagen").src="images".concat("/IMG",index,".jpg")
+  document.getElementById("circle".concat(index)).classList.add("circle-fill");
+}
 
 function menu(){
     console.log("Holita")
@@ -8,7 +40,7 @@ function menu(){
         for (var i=0;i<document.getElementsByClassName("menu-items").length;i+=1){
             document.getElementsByClassName("menu-items")[i].style.display = 'block';
         }
-        document.getElementsByTagName('header')[0].style.height='233px'
+        document.getElementsByTagName('header')[0].style.height='265px'
         flag_menu=1
     }else{
         for (var i=0;i<document.getElementsByClassName("menu-items").length;i+=1){
@@ -26,9 +58,6 @@ function screenTest(e) {
     /* the viewport is 600 pixels wide or less */
     //para.textContent = 'This is a narrow screen — less than 600px wide.';
     //document.body.style.backgroundColor = 'red';
-    for (var i=0;i<document.getElementsByClassName("menu-items").length;i+=1){
-      document.getElementsByClassName("menu-items")[i].style.display = 'none';
-  }
   } else {
     /* the viewport is more than than 600 pixels wide */
     //para.textContent = 'This is a wide screen — more than 600px wide.';
@@ -39,6 +68,7 @@ function screenTest(e) {
         document.getElementsByClassName("menu-items")[i].style.display = 'block';
     }
     document.getElementsByTagName('header')[0].style.height='105px'
+    document.getElementsByClassName("btn-res-log")[0].style.display = 'none';
   }
 }
 
